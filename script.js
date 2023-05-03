55d651a6b2200838d3e85d845f7357f88f5b65d8
SOSO <128017179+COoOnjay@users.noreply.github.com>
2023-05-03 19:58:23 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+// Smooth scroll
+const links = document.querySelectorAll('nav a');
+
+links.forEach(link => {
+  link.addEventListener('click', e => {
+    e.preventDefault();
+    const href = link.getAttribute('href');
+    const offsetTop = document.querySelector(href).offsetTop;
+
+    scroll({
+      top: offsetTop,
+      behavior: 'smooth'
+    });
+  });
+});
